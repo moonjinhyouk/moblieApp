77f880f7086badc6887558aa5c660384e383a206
moonjinhyouk <100898147+moonjinhyouk@users.noreply.github.com>
2025-04-18 15:28:42 +0900

    Add files via upload

---
+++ 0418/menu/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.meun;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    View view1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        view1 = findViewById(R.id.layout);
+    }
+
+    @Override
+    public  boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.mymenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if( id == R.id.blue){
+            view1.setBackgroundColor(Color. BLUE);
+            return true;
+        }
+        else if( id == R.id.green){
+            view1.setBackgroundColor(Color.GREEN);
+            return true;
+        }
+        else if( id == R.id.red){
+            view1.setBackgroundColor(Color.RED);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
