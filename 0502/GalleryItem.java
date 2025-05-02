4f83a26c9c0a7b3326ecb0556ffe36759b43d5b9
moonjinhyouk <100898147+moonjinhyouk@users.noreply.github.com>
2025-05-02 15:59:09 +0900

    Add files via upload

---
+++ 0502/GalleryItem.java
@@ -0,0 +1,13 @@
+package com.example.roffjfl;
+
+
+
+public class GalleryItem {
+    public String title;
+    public int imageResId;
+
+    public GalleryItem(String title, int imageResId) {
+        this.title = title;
+        this.imageResId = imageResId;
+    }
+}
